20c409b75734447fdb971e2f885e748bbf7f8519
takanori-hagiwara <allhappyhagi@outlook.com>
2021-03-23 21:48:44 +0900

    database.yml編集

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,7 +17,7 @@ listen 3000
 stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 #通常のログを記録するファイルを指定
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+stderr_path "#{app_path}/log/unicorn.stderr.log"
 
 #Railsアプリケーションの応答を待つ上限時間を設定
 timeout 60
